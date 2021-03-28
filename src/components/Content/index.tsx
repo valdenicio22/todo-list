f7a41fec18766eb543514f6f26820b286dda2a96
valdenicio22 <valdenicio22@gmail.com>
2021-03-27 23:07:58 -0300

    getting started

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,15 @@
+import './styles.css';
+
+export function Content(){
+    return (
+        <div className="containerMain">
+            <h1>Tasks</h1>
+            <input type="text"/>
+            <ul>
+                <li>Create new project</li>
+                <li>Working Call</li>
+                <li>Meet with doctor</li>
+            </ul>
+        </div>
+    );
+} 
